8ca6b631dc4df9a454d14417d831562678df01e0
VERSCHELDE <pieter.verschelde@inbo.be>
2021-02-02 17:36:47 +0000

    controlekaarten met fixed grenzen

--- inbolimsintern/R/qcc_gg_lims_shewhart.R
+++ inbolimsintern/R/qcc_gg_lims_shewhart.R
@@ -5,25 +5,26 @@
 #' @param red_rules numeric vector of rule violations that should be marked as red
 #' @param orange_rules numeric vector of rule violations that should be marked as orange
 #' @param yellow_rules numeric vector of rule violations that should be marked as yellow
+#' @param add_nominal logical indicating if a theoretical value should also be plotted (in the column with name nominal_value)
 #' @param ... not currently in use
-#' @importFrom ggplot2 ggplot geom_line geom_point aes xlab ylab 
+#' @importFrom ggplot2 ggplot geom_line geom_point aes xlab ylab
 #' @importFrom rlang .data
-#' @return Shewhart ggplot 
+#' @return Shewhart ggplot
 #' @export
-gg_lims_shewhart <- function(plotdata, red_rules = 1:2, orange_rules = 3:6, yellow_rules = 7:8, ...){
+gg_lims_shewhart <- function(plotdata, red_rules = 1:2, orange_rules = 3:6, yellow_rules = 7:8, add_nominal = TRUE, ...){
   if (!all(red_rules, orange_rules, yellow_rules) %in% 1:8) stop("chosen rules not valid, should be a vector of integers between 1 and 8")
   if (length(red_rules)) {
     plotdata$RR <- as.numeric(rowSums(plotdata[, paste0("rule", sprintf("%02d",red_rules))]) > 0)
   } else {
     plotdata$RR <- 0
   }
-  
+
   if (length(orange_rules)) {
     plotdata$OR <- as.numeric(rowSums(plotdata[, paste0("rule", sprintf("%02d",orange_rules))]) > 0)
   } else {
     plotdata$OR <- 0
   }
-  
+
   if (length(yellow_rules)) {
     plotdata$YR <- as.numeric(rowSums(plotdata[, paste0("rule", sprintf("%02d",yellow_rules))]) > 0)
   } else {
@@ -31,23 +32,26 @@ gg_lims_shewhart <- function(plotdata, red_rules = 1:2, orange_rules = 3:6, yell
   }
   plotdata$color <- ifelse(plotdata$RR, "red", ifelse(plotdata$OR, "orange", ifelse(plotdata$YR, "yellow", "black")))
   plotdata
-  
+
   value <- color <- Nr <- NULL #to avoid NOTE in as-cran
-  g <- 
+  g <-
     ggplot(plotdata, aes(x = .data$Nr, y = .data$value)) +
-    geom_line(aes(y = .data$lcl_1s), lty = 2, color = "gold") + 
+    geom_line(aes(y = .data$lcl_1s), lty = 2, color = "gold") +
     geom_line(aes(y = .data$ucl_1s), lty = 2, color = "gold") +
-    geom_line(aes(y = .data$lcl_2s), lty = 2, color = "orange") + 
+    geom_line(aes(y = .data$lcl_2s), lty = 2, color = "orange") +
     geom_line(aes(y = .data$ucl_2s), lty = 2, color = "orange") +
-    geom_line(aes(y = .data$lcl_3s), lty = 2, color = "red") + 
-    geom_line(aes(y = .data$ucl_3s), lty = 2, color = "red") +
-    geom_line(aes(y = .data$center)) +
+    geom_line(aes(y = .data$lcl_3s), lty = 2, color = "red") +
+    geom_line(aes(y = .data$ucl_3s), lty = 2, color = "red")
+    if (!is.null(plotdata$nominal_value) & add_nominal == TRUE) {
+      g <- g + geom_line(aes(y = .data$nominal_value), lty = 3, color = "green3")
+    }
+  g <- g +
+    geom_line(aes(y = .data$center), lty = 2, color = "darkblue") +
     geom_line(lty = 3) +
     geom_point(data = subset(plotdata, color == "black"), color = "darkgreen") +
     geom_point(data = subset(plotdata, color == "red"), color = "red", size = rel(3)) +
     geom_point(data = subset(plotdata, color == "orange"), color = "orange", size = rel(2)) +
     geom_point(data = subset(plotdata, color == "yellow"), color = "yellow") +
     xlab("") + ylab("")
-  
   g
 }
