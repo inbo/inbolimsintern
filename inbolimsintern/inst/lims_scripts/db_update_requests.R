8880afaa0bf80d09963177c747ac0e38a0afa92c
VERSCHELDE <pieter.verschelde@inbodev.be>
2025-08-19 22:17:06 +0000

    update script for db_update_requests naar lims8

--- inbolimsintern/inst/lims_scripts/db_update_requests.R
+++ inbolimsintern/inst/lims_scripts/db_update_requests.R
@@ -1,40 +1,76 @@
 #Requested DB updates
+#
+#voorbeeldcsv (semicolon-delimited)
+#LabSampleID;FieldSampleID;FieldSamplingDate;veld_te_wijzigen;LIMSAnalysisName;originele_waarde;nieuwe_waarde;opmerking_MilKlim;partim
+#24-007337;K12;7/04/2024 0:00;FieldSampleID;NA;K12;KESR009X;FieldSampleID aanpassen;sample
+#24-007344;Z6;7/04/2024 0:00;FieldSampleID;NA;Z6;KESR005X;FieldSampleID aanpassen;sample
 
-### R libraries
-library(inbolimsintern)
-library(DBI)
+#
+#
+#// setup environment
+##=====================
+
 library(tidyverse)
+library(inbolimsintern)
+library(htmltools) #must be loaded for tags functionality
+fig_height <- 600
 
-### Logfile
-logfile <- logfile_start(prefix = "REQUESTED_DB_UPDATES")
-writeLines(con = logfile, paste0("REQUESTED_DB_UPDATES\n-------------\ninbolimsintern versie: ", packageVersion("inbolimsintern")))
+args <- commandArgs(trailingOnly = TRUE)
+#args <- c("LWL8DEV", "", "TEST_INT")
 
-### LIMS argumenten
-call_id <- 0 #call_id <- 2252 #2186 #2181 #2138 #2128 #1647 1982 6021
-try({
-  args <- inbolimsintern::prepare_session(call_id)
-  conn <- inbolimsintern::limsdb_connect(uid = args["uid"], pwd = args["pwd"])
-  params <- inbolimsintern::read_db_arguments(conn, args["call_id"])
-}, outFile = logfile)
+username <- args[3]
+call_id <- args[2]
+odbc <- args[1]
+
+message("call_id = ", call_id)
+setup <- try(r_session_setup(call_id, odbc))
+if (inherits(setup, "try-error")) {
+  stop("probleem bij setup script")
+}
+invisible(list2env(setup, envir = .GlobalEnv))
+message("session prepared")
+
+status <- inbolimsintern::read_db_log(conn, call_id)
+write_db_log(conn, call_id, "P", "Started db update requests", user = username)
+
 
 file_name <- params %>% filter(ARG_NAME =="FILE") %>% pull(VALUE)
 gebruiker <- params %>% filter(ARG_NAME =="USER") %>% pull(VALUE)
 
-#Lees de csv file in
-try({
+
+#//Read csv file
+
+e <- try({
   dfChanges <- read_csv2(file_name, guess_max = 100000)
 } , outFile = logfile)
-cat(nrow(dfChanges), ' records\n', file = logfile, append = TRUE)
+if (inherits(e, "try-error")) {
+  write_db_log(conn, call_id, "E", paste("csv file could not be read:", e), user = username)
+} else {
+  write_db_log(conn, call_id, "P", paste("csv read:", nrow(dfChanges), " records"), user = username)
+}
 colnames(dfChanges) <- toupper(colnames(dfChanges))
 
 #Zet de csv om naar  bruikbaar data formaat
-dfUpdates <- dfChanges %>%
+e <- try({
+  dfUpdates <- dfChanges %>%
   rowwise() %>%
   do(db_update_get_data(., conn = conn))
+})
+if (inherits(e, "try-error")) {
+  write_db_log(conn, call_id, "E", paste("Error converting data:", e), user = username)
+} else {
+  write_db_log(conn, call_id, "P", paste("updates to be processed:", nrow(dfUpdates)), user = username)
+}
 
 #Maak de queries aan voor de logtabel
 queries <- db_update_worklist_table(conn, as.data.frame(dfUpdates))
-sapply(queries, dbGetQuery, conn = conn)
+e <- try(sapply(queries, DBI::dbGetQuery, conn = conn))
+if (inherits(e, "try-error")) {
+  write_db_log(conn, call_id, "E", e, user = username)
+} else{
+  write_db_log(conn, call_id, "C", 'updates added to the update table', user = username)
+}
+
 
 
 
